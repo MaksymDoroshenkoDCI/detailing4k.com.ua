b5a7910f32f825a49c8c5901b8be8e9675e791a1
doroshenkomax <doroshenko@fintom8.com>
2025-12-29 20:23:14 +0100

    Оновлено ESLint конфігурацію: вимкнено react/no-unescaped-entities та змінено img на warn

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
